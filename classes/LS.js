de1cd491c79ad4fb581594c4ce3546a03b13608b
Gert-Ranno Lumine <gert-ranno.kask@khk.ee>
2021-12-08 11:27:28 +0200

    Refactored the code

--- classes/LS.js
+++ classes/LS.js
@@ -1,13 +1,21 @@
 class LS{
+    getData(key){
+        let data
+        if(localStorage.getItem(key) === null) {
+            data = []
+
+        } else {
+            data = JSON.parse(localStorage.getItem(key))
+        }
+        return  data
+    }
+    setData (key, data){
+        localStorage.setItem(key, JSON.stringify(data))
+    }
+
     addBook(book){
-        let books
-        if(localStorage.getItem('books') === null) {
-            books = []
-        }
-        else {
-            books = JSON.parse(localStorage.getItem('books'))
-        }
+        const books = this.getData('books')
         books.push(book)
-        localStorage.setItem('books', JSON.stringify(books))
+        this.setData('books', books)
     }
 }
