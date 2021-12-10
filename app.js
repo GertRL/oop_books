423319cbbd3d22cdb45837be88d1c4d21f9d1024
Gert-Ranno Lumine <gert-ranno.kask@khk.ee>
2021-12-10 09:26:43 +0200

    kustutab mälust raamatud

--- app.js
+++ app.js
@@ -1,39 +1,54 @@
-// add object
+// app objects
 const ui = new UI()
 const ls = new LS()
 
-const form =document.querySelector('form')
-form.addEventListener('submit',addBook)
+// user input form
+const form = document.querySelector('#book-form')
+form.addEventListener('submit', addBook)
 
-function addBook(event) {
-    // get task value from form input
-    const title = ui.title.value
-    const author = ui.author.value
-    const isbn = ui.isbn.value
-
-    //// create book abject with user data
-    const book = new Book(title,author,isbn)
-    console.log(book)
-
-    /// add book data to ui and show it
-    ui.addBook(book)
-    // Save book data to LS
-    ls.addBook(book)
-
-    // clear form input value
-
-    ui.title.value
-    ui.author.value
-    ui.isbn.value
-
-    event.preventDefault()
-}
-
+// page reload
 document.addEventListener('DOMContentLoaded', getBooks)
 
+// books table click event
+bookList = document.querySelector('#book-list')
+bookList.addEventListener('click', delBook)
+
+function delBook(event){
+    if(event.target.textContent === 'X'){
+        const book = ui.getBook(event.target)
+        if(ui.delBook(event.target) === true){
+            ls.delBook(book)
+        }
+    }
+}
+
+
+
 function getBooks(){
+    // get data from LS
     const books = ls.getData('books')
+    // for each book in books
     books.forEach(function (booksFromLS){
         ui.addBook(booksFromLS)
     })
+}
+
+// addBook function
+function addBook(event){
+    // get form data from form input
+    const title = ui.title.value
+    const author = ui.author.value
+    const isbn = ui.isbn.value
+    // create book object with user data
+    const book = new Book(title, author, isbn)
+    console.log(book)
+    // add book data to ui and show it
+    ui.addBook(book)
+    // save book data to ls
+    ls.addBook(book)
+    // clear form input value
+    ui.title.value = ''
+    ui.author.value = ''
+    ui.isbn.value = ''
+    event.preventDefault()
 }
--- classes/Book.js
+++ classes/Book.js
@@ -1,6 +1,6 @@
 class Book{
     constructor(title, author, isbn) {
-        this.title =title
+        this.title = title
         this.author = author
         this.isbn = isbn
     }
